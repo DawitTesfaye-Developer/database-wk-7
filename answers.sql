7052dad0d6b908b79f47cd2b7d9d84ced43ba23c
DAWIT TESFAYE <123860794+DawitTesfaye-Developer@users.noreply.github.com>
2025-09-18 16:57:00 -0400

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -40,3 +40,28 @@ INSERT INTO OrderItems (OrderID, Product, Quantity) VALUES
 (102, 'Keyboard', 1),
 (102, 'Mouse', 2),
 (103, 'Phone', 1);
+
+--Question 1.
+
+-- Create a new table for normalized data
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(50),
+    Product VARCHAR(50)
+);
+
+-- Insert normalized rows
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product) VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+-- Check result
+SELECT * FROM ProductDetail_1NF;
+
+
+
+
